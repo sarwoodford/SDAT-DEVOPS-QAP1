63f0d8fe4b97c0a15d8b49d204216173d9cbfdfd
sara woodford <sarawoodford6@gmail.com>
2025-02-06 11:07:06 -0330

    feat: Book class
    - basic class to initialize fields, implement
    setters and getters
    - book methods will be implemented in LibraryManager

---
+++ Library/BookManager.java
@@ -0,0 +1,71 @@
+package Library;
+
+import java.util.Date;
+
+public class BookManager {
+    private int bookID;
+    private String bookTitle;
+    private Date borrowDate;
+    private Date returnDate;
+    private int availableCopies;
+    private boolean isAvailable;
+
+    public BookManager(int bookID, String bookTitle, Date borrowDate, Date returnDate, int availableCopies, boolean isAvailable){
+        this.bookID = bookID;
+        this.bookTitle = bookTitle;
+        this.borrowDate = borrowDate;
+        this.returnDate = returnDate;
+        this.availableCopies = availableCopies;
+        this.isAvailable = true;
+    }
+
+    public int getBookID(){
+        return bookID;
+    }
+
+    public String getBookTitle(){
+        return bookTitle;
+    }
+
+    public Date getBorrowDate(){
+        return borrowDate;
+    }
+
+    public Date getReturnDate(){
+        return returnDate;
+    }
+
+    public int getAvailableCopies(){
+        return availableCopies;
+    }
+
+    public boolean getIsAvailable(){
+        return isAvailable;
+    }
+
+    public void setBookID(int bookID){
+        this.bookID = bookID;
+    }
+
+    public void setBookTitle(String bookTitle){
+        this.bookTitle = bookTitle;
+    }
+
+    public void setBorrowDate(Date borrowDate){
+        this.borrowDate = borrowDate;
+    }
+
+    public void setReturnDate(Date returnDate){
+        this.returnDate = returnDate;
+    }
+
+    public void setAvailableCopies(int availableCopies){
+        this.availableCopies = availableCopies;
+    }
+
+    public void setIsAvailable(boolean isAvailable){
+        this.isAvailable = isAvailable;
+    }
+}
+
+
--- Library/LibraryManager.java
+++ Library/LibraryManager.java
@@ -1,3 +1,4 @@
+package Library;
 public class LibraryManager{
     
 }
