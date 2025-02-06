3c04f22af51af91944a8f78cfeba6386a99525b5
sara woodford <sarawoodford6@gmail.com>
2025-02-06 14:57:33 -0330

    feat: LibraryManager
    methods for CLI interface:
    - add book
    - borrow book (assigned to user)
    - return book (assigned to user)
    -search book by title
    - search all books (catalog)
    -add user

--- Library/LibraryManager.java
+++ Library/LibraryManager.java
@@ -1,4 +1,57 @@
 package Library;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 public class LibraryManager{
-    
+    private List<BookManager> books;
+    private List<UserManager> users;
+
+    public LibraryManager(){
+        this.books = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+
+    public void addBook(BookManager book){
+        books.add(book);
+    }
+
+    public boolean borrowBook(UserManager user, BookManager book){
+        if(book.getIsAvailable() && user.booksBorrowedCount() < 3){
+            user.getBorrowedBooks().add(book);
+            book.setIsAvailable(false);
+            return true;
+        }
+
+        System.out.println("The selected book is unavailable, or you have reached your borrowing limit (3).");
+        return false;
+    }
+
+    public boolean returnBook(UserManager user, BookManager book){
+        if(user.getBorrowedBooks().contains(book)){
+            user.getBorrowedBooks().remove(book);
+            book.setIsAvailable(true);
+            return true;
+        }
+
+        System.out.println("The entered book has not been borrowed by you.");
+        return false;
+    }
+
+    public List<BookManager> searchBookTitle(String bookTitle){
+        return books.stream()
+        .filter(book -> book.getBookTitle().toLowerCase().contains(bookTitle.toLowerCase())).collect(Collectors.toList());
+    }
+
+    public List<BookManager> searchBookCatalog(){
+        return new ArrayList<>(books);
+    }
+
+    public void addUser(UserManager user){
+        if (!users.contains(user)){
+            users.add(user);
+        } else {
+            System.out.println("A user with these credentials already exists!");
+        }
+    }
 }
