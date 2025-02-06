d96d1703b98f9a8ae2f9b9ab47f529fe7bf83489
sara woodford <sarawoodford6@gmail.com>
2025-02-06 11:22:48 -0330

    feat: UserManager class
    - basic class with constructor, setters and getters
    - method to count how many books user has
    borrowed
    - method to check if a user has borrowed a
    specific book

--- Library/UserManager.java
+++ Library/UserManager.java
@@ -1,5 +1,48 @@
 package Library;
 
+import java.util.List;
+import java.util.ArrayList;
 public class UserManager {
-    
+    private int userID;
+    private String userName;
+    private List<BookManager> borrowedBooks;
+
+    public UserManager(int userID, String userName, List<BookManager> borrowedBooks){
+        this.userID = userID;
+        this.userName = userName;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public int getUserID(){
+        return userID;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public List<BookManager> getBorrowedBooks(){
+        return borrowedBooks;
+    }
+
+    public void setUserId(int userID){
+        this.userID = userID;
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public void setBorrowedBooks(List<BookManager> borrowedBooks){
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    public int booksBorrowedCount(){
+        return borrowedBooks.size();
+    }
+
+    public boolean hasBorrowedBooks(BookManager book){
+        return borrowedBooks.contains(book);
+    }
+
 }
