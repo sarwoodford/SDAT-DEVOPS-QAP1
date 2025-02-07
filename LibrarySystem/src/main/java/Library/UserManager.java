1bdfad30e5ca6023a1b1a034f796b10173fb49e1
sara woodford <sarawoodford6@gmail.com>
2025-02-07 18:42:40 -0330

    docs: adding javadocs to classes

--- LibrarySystem/src/main/java/Library/UserManager.java
+++ LibrarySystem/src/main/java/Library/UserManager.java
@@ -2,48 +2,97 @@ package LibrarySystem.src.main.java.Library;
 
 import java.util.List;
 import java.util.ArrayList;
+
 public class UserManager {
     private String userID;
     private String userName;
     private List<BookManager> borrowedBooks;
 
-    public UserManager(String userID, String userName, List<BookManager> borrowedBooks){
+    /**
+     * UserManager constructor
+     * 
+     * @param userID
+     * @param userName
+     * @param borrowedBooks
+     */
+    public UserManager(String userID, String userName, List<BookManager> borrowedBooks) {
         this.userID = userID;
         this.userName = userName;
         this.borrowedBooks = new ArrayList<>();
     }
 
-    public String getUserID(){
+    /**
+     * get user id
+     * 
+     * @return userID
+     */
+    public String getUserID() {
         return userID;
     }
 
-    public String getUserName(){
+    /**
+     * get user name
+     * 
+     * @return userName
+     */
+    public String getUserName() {
         return userName;
     }
 
-    public List<BookManager> getBorrowedBooks(){
+    /**
+     * list of books borrowed by users
+     * 
+     * @return borrowedBooks
+     */
+    public List<BookManager> getBorrowedBooks() {
         return borrowedBooks;
     }
 
-    public void setUserId(String userID){
+    /**
+     * set user ID
+     * 
+     * @param userID
+     */
+    public void setUserId(String userID) {
         this.userID = userID;
     }
 
-    public void setUserName(String userName){
+    /**
+     * set user name
+     * 
+     * @param userName
+     */
+    public void setUserName(String userName) {
         this.userName = userName;
     }
 
-    public void setBorrowedBooks(List<BookManager> borrowedBooks){
+    /**
+     * set books borrowed by user
+     * 
+     * @param borrowedBooks
+     */
+    public void setBorrowedBooks(List<BookManager> borrowedBooks) {
         this.borrowedBooks = borrowedBooks;
     }
 
-    public int booksBorrowedCount(){
+    /**
+     * method to track books borrowed by user to verify they
+     * dont exceed their borrowing limit (3)
+     * 
+     * @return
+     */
+    public int booksBorrowedCount() {
         return borrowedBooks.size();
     }
 
-    public boolean hasBorrowedBooks(BookManager book){
+    /**
+     * checks if user has borrowed a specifc book
+     * 
+     * @param book
+     * @return boolean
+     */
+    public boolean hasBorrowedBooks(BookManager book) {
         return borrowedBooks.contains(book);
     }
 
-    
 }
